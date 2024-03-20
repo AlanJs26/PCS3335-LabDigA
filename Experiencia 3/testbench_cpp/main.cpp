a2054ddfd5811de0979bf61d85c0b9b153e8bc99
AlanJS26 <alanjoses.29@gmail.com>
2024-03-19 22:08:51 -0300

    🐛 use unsigned numbers

--- Experiencia 3/testbench_cpp/main.cpp
+++ Experiencia 3/testbench_cpp/main.cpp
@@ -9,7 +9,7 @@
 #include <iomanip>
 #include <stdint.h>
 
-int32_t reverse(int32_t x)
+uint32_t reverse(uint32_t x)
 {
     x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1); // Swap _<>_
     x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2); // Swap __<>__
@@ -19,68 +19,68 @@ int32_t reverse(int32_t x)
     return x;
 }
 
-int32_t rotr(int32_t x, int n){
+uint32_t rotr(uint32_t x, int n){
     return (x >> n) | (x << (32 - n));
 }
 
-int32_t ch(int32_t x,int32_t y,int32_t z) {
+uint32_t ch(uint32_t x,uint32_t y,uint32_t z) {
   return (x & y) ^ (~x & z);
 }
 
-int32_t maj(int32_t x,int32_t y,int32_t z) {
+uint32_t maj(uint32_t x,uint32_t y,uint32_t z) {
   return (x & y) ^ (x & z) ^ (y & z);
 }
 
-int32_t sum0(int32_t x) {
+uint32_t sum0(uint32_t x) {
   return rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22);
 }
 
-int32_t sum1(int32_t x) {
+uint32_t sum1(uint32_t x) {
   return rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25);
 }
 
-int32_t sigma0(int32_t x) {
+uint32_t sigma0(uint32_t x) {
   return rotr(x, 7) ^ rotr(x, 18) ^ (x >> 3);
 }
 
-int32_t sigma1(int32_t x) {
+uint32_t sigma1(uint32_t x) {
   return rotr(x, 17) ^ rotr(x, 19) ^ (x >> 10);
 }
 
-void printbinary(int32_t x, std::string name){
+void printbinary(uint32_t x, std::string name){
     std::cout << std::dec << std::left << name << ": " << std::setw(20) << x << std::right << "  binario: " << std::bitset<32>(x) << "  hexadecimal: " << std::hex << x << std::endl;
 }
 
 void stepfun(
-    int32_t ai,
-    int32_t bi,
-    int32_t ci,
-    int32_t di,
-    int32_t ei,
-    int32_t fi,
-    int32_t gi,
-    int32_t hi,
-    int32_t kpw
+    uint32_t ai,
+    uint32_t bi,
+    uint32_t ci,
+    uint32_t di,
+    uint32_t ei,
+    uint32_t fi,
+    uint32_t gi,
+    uint32_t hi,
+    uint32_t kpw
     ){
 
-    int32_t a0 = hi + sum1(ei) + ch(ei,fi,gi) + kpw + sum0(ai) + maj(ai,bi,ci);
-    int32_t b0 = ai;
-    int32_t c0 = bi;
-    int32_t d0 = ci;
-    int32_t e0 = di + hi + sum1(ei) + ch(ei,fi,gi) + kpw;
-    int32_t f0 = ei;
-    int32_t g0 = fi;
-    int32_t h0 = gi;
+    uint32_t a0 = hi + sum1(ei) + ch(ei,fi,gi) + kpw + sum0(ai) + maj(ai,bi,ci);
+    uint32_t b0 = ai;
+    uint32_t c0 = bi;
+    uint32_t d0 = ci;
+    uint32_t e0 = di + hi + sum1(ei) + ch(ei,fi,gi) + kpw;
+    uint32_t f0 = ei;
+    uint32_t g0 = fi;
+    uint32_t h0 = gi;
 
-    printbinary(ai, "ai");
-    printbinary(bi, "bi");
-    printbinary(ci, "ci");
-    printbinary(di, "di");
-    printbinary(ei, "ei");
-    printbinary(fi, "fi");
-    printbinary(gi, "gi");
-    printbinary(hi, "hi");
-
+    // printbinary(ai, "ai");
+    // printbinary(bi, "bi");
+    // printbinary(ci, "ci");
+    // printbinary(di, "di");
+    // printbinary(ei, "ei");
+    // printbinary(fi, "fi");
+    // printbinary(gi, "gi");
+    // printbinary(hi, "hi");
+    //
     std::cout << std::endl;
 
     printbinary(a0, "a0");
@@ -94,7 +94,7 @@ void stepfun(
 
 }
 
-void printOperacao(int32_t x, std::string nome){
+void printOperacao(uint32_t x, std::string nome){
     std::cout << "*" << nome << "*" << "\n\n";
 
     std::bitset<32> binary(x);
@@ -105,17 +105,11 @@ void printOperacao(int32_t x, std::string nome){
 
 int main()
 {
-    int32_t x = 0b10101010;
-    x = x | (x << 8) | (x << 16) | (x << 24);
-    int32_t y = ~x;
-    int32_t z = reverse(x);
+    uint32_t x = 0b10000001; // número de 32 bits com sinal
 
-    int32_t sum0Result = sum0(x); 
-    int32_t sum1Result = sum1(x); 
-    int32_t sigma0Result = sigma0(x); 
-    int32_t sigma1Result = sigma1(x); 
-    int32_t chResult = ch(x,y,z); 
-    int32_t majResult = maj(x,y,z); 
+    x = x | (x << 8) | (x << 16) | (x << 24);
+    uint32_t y = ~x;
+    uint32_t z = reverse(x);
 
     std::cout << "x: " << x << std::endl;
     std::bitset<32> binary_x(x);
@@ -129,6 +123,13 @@ int main()
     std::bitset<32> binary_z(z);
     std::cout << "z: " << binary_z << std::endl << std::endl;
 
+    // uint32_t sum0Result = sum0(x); 
+    // uint32_t sum1Result = sum1(x); 
+    // uint32_t sigma0Result = sigma0(x); 
+    // uint32_t sigma1Result = sigma1(x); 
+    // uint32_t chResult = ch(x,y,z); 
+    // uint32_t majResult = maj(x,y,z); 
+
     // printOperacao(sum0Result, "sum0");
     // printOperacao(sum1Result, "sum1");
     // printOperacao(sigma0Result, "sigma0");
@@ -136,19 +137,17 @@ int main()
     // printOperacao(chResult, "ch");
     // printOperacao(majResult, "maj");
 
-
-
     stepfun(
             x,
-            y,
-            z,
             x,
-            y,
-            z,
             x,
-            y,
+            x,
+            x,
+            x,
+            x,
+            x,
     
-            y
+            x
            );
 
     return 0;

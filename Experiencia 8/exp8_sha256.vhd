f21f743837bf738e8177060bb769f5e3d62ed5c4
AlanJS <alanjoses.29@gmail.com>
2024-05-07 17:00:08 -0300

    Update exp8_sha256.vhd

--- Experiencia 8/exp8_sha256.vhd
+++ Experiencia 8/exp8_sha256.vhd
@@ -1,68 +1,62 @@
 
-entity exp7_sha256_1b_top is
-    port (
-        -- SW : in bit_vector(9 downto 0);
-        CLOCK_50 : in bit;
-        GPIO_0 : in bit_vector(35 downto 0);
-        GPIO_1 : out bit_vector(35 downto 0)
-        -- KEY : in bit_vector(3 downto 0);
-        -- LEDR : out bit_vector(9 downto 0);
-        -- HEX0 : out bit_vector(7 downto 0);
-        -- HEX1 : out bit_vector(7 downto 0);
-        -- HEX2 : out bit_vector(7 downto 0);
-        -- HEX3 : out bit_vector(7 downto 0);
-        -- HEX4 : out bit_vector(7 downto 0);
-        -- HEX5 : out bit_vector(7 downto 0)
+entity exp8_sha256_top is
+  port (
+      CLOCK_50 : in bit;
+      LEDR : out bit_vector(9 downto 0);
+      GPIO_0 : in bit_vector(35 downto 0);
+      GPIO_1 : out bit_vector(35 downto 0)
+  );
+end exp8_sha256_top;
+
+architecture arch of exp8_sha256_top is
+
+
+  component clock_diviser is
+    generic (
+      CLOCK_MUL : integer
     );
-  end exp7_sha256_1b_top;
-  
-  architecture arch of exp7_sha256_1b_top is
+    port (
+      i_clk : in bit;
+      i_rst : in bit;
+      o_clk_div : out bit
+    );
+  end component;
 
-    component sha256_1b is
-        port (
-            clock, reset : in bit; -- Clock da placa, GPIO_0_D2  
-            serial_in : in bit; -- GPIO_0_D0
-            serial_out : out bit -- GPIO_0_D1
-        );
-    end component;
+  component sha256 is
+    port (clock, reset : in  bit;		-- Clock da placa, GPIO_0_D2  
+            serial_in    : in  bit;		-- GPIO_0_D0
+            LEDR : out bit_vector(9 downto 0);
+            serial_out	 : out bit		-- GPIO_0_D1
+    );
+  end component;
+
+  signal clock : bit;
+
+  signal reset, serial_in, serial_out : bit;
 
 
-    component clock_diviser is
-      generic (
-        CLOCK_MUL : integer
-      );
-      port (
-        i_clk : in bit;
-        i_rst : in bit;
-        o_clk_div : out bit
-      );
-    end component;
-  
-    signal clock, reset, clock_19200 : bit;
-  
-    signal serial_in, serial_out : bit;
-  
-  begin
-  
-    reset <= GPIO_0(0); -- GPIO 6;  
-    serial_in <= GPIO_0(1); -- GPIO 7;
-    GPIO_1(1) <= serial_out; -- GPIO 5;
+begin   
 
-    clock <= CLOCK_50;
-  
-  
-    sha256 : sha256_1b port map(clock_19200, reset, serial_in, serial_out);
+  SHA256_IMPL: sha256 port map(clock, reset, serial_in, LEDR, serial_out);
 
-    CLOCK_DIVISER_4800 : clock_diviser
-    generic map (
-      CLOCK_MUL => (50000000/19200)/2 
-    )
-    port map(clock, '0', clock_19200);
+  reset <= GPIO_0(1); -- pino 15
   
-    LEDR <= (others=>'1');
-  
-  end architecture;
-  
+  serial_in <= GPIO_0(0); -- pino 7
+  GPIO_1(0) <= serial_out; -- pino 6
+
+  CLOCK_DIVISER_INSTANCE : clock_diviser
+  generic map(
+    -- CLOCK_MUL => 1301
+    CLOCK_MUL => (50000000/19200)/2 
+  )
+  port map(
+      CLOCK_50, '0', clock
+  );
+
+
+
+end architecture;
+
 
   -------   DIVISOR DE CLOCK 
 
